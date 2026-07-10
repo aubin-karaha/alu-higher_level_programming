efebdc67b979bc91af4cfc7d3e3fe61b50a05b3e
aubin-karaha <c.karaha@alustudent.com>
2026-07-10 08:18:27 -0700

    Adding corrected Task-0 file

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-last_digit = number % 10
-if number < 0 and last_digit != 0:
-    last_digit -= 10
 
-print("Last digit of {:d} is {:d}".format(number, last_digit), end=" ")
+number = random.randint(-10, 10)
 
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+if number > 0:
+    print("{:d} is positive".format(number))
+elif number == 0:
+    print("{:d} is zero".format(number))
 else:
-    print("and is less than 6 and not 0")
+    print("{:d} is negative".format(number))
