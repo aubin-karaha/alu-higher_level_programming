2e915ecfa0be9a2540a594b88285af81e1e61dbf
aubin-karaha <c.karaha@alustudent.com>
2026-07-10 08:57:47 -0700

    Fix task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print("00", end="")
-for i in range(1, 100):
-    print(", {:02d}".format(i), end="")
-print("")
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
