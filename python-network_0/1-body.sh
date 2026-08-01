425a97c5b8462fda062330af479895ae75a0497f
aubin-karaha <c.karaha@alustudent.com>
2026-08-01 04:59:43 +0200

    Fix 4-header.sh to use correct header name X-School-User-Id

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request and displays the body only if the status is 200
-curl -s -o /tmp/body_output -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body_output
+# sends a GET request, follows redirects, and displays the body only if final status is 200
+curl -s -L -o /tmp/body_output -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body_output
