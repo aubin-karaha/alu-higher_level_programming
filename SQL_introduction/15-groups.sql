ad59fd7c4d7f65b4068ed41dfeb1ca03ec0221fe
aubin-karaha <c.karaha@alustudent.com>
2026-08-01 02:46:02 +0200

    Add missing task 15-groups.sql

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- lists the number of records for each score in second_table,
+-- ordered by number of records (highest first)
+SELECT score, COUNT(*) AS number FROM second_table
+    GROUP BY score ORDER BY number DESC;
